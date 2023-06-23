ea17b65c47148c9729a57c2192c686be4be2695e
Kevin Tu <tukevin274@gmail.com>
2023-06-22 22:35:59 -0600

    Added more details in definitions
    I added so that it prints out examples of the word depending on the context/definition as given.
    Additionally, I found that some words can generate an absurd amount of definitions (I've seen 12 definitions before), so I decided to cut it down to a strict 4.
    Also, new README file!!!

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# pdf-dictionary
+# PDF Dictionary
+
+The purpose of this app is to provide users the ability to quickly look up the definition of a word without exiting tabs to search it up in Google.
+
+## About
+
+Our current goal is to be able to implement the app as a nonprofit, opensource google extension to use along side the default PDF reader in google chrome.
+
+Eventually, we are looking towards being able to provide definitions for usage on websites, and not limited to PDF files.
+
+- This project is for educational and experimental purposes.
--- definition-retriever.py
+++ definition-retriever.py
@@ -1,7 +1,8 @@
 import requests
+
 def get_definition(word):
     """
-    returns a definition from a user input
+    returns all the details of the given word
     """
     Base_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/"
     # sends a request to the dictionary API and stores the dictionary
@@ -17,10 +18,19 @@ def get_definition(word):
         for item in meanings:
             print(item["partOfSpeech"].capitalize()+ ":")
             for number, definitions in enumerate(item["definitions"]):
-               print(f"{number+1}. {definitions['definition']}")
-            print("\n")
 
-
+                if number and len(definitions) == 4:
+                    # different partOfSpeech may not have any examples.
+                    print(f"{number+1}. {definitions['definition']}")
+                    print(f"Example: {definitions['example']}")
+                    print("\n")
+                elif number > 4:
+                    # Some words may have many defs which will get redundant
+                    break
+                else:
+                    print(f"{number+1}. {definitions['definition']}")
+                    print("\n")
+        
 def main():
     word = input("Enter a Word: ")
     get_definition(word)
