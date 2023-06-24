d38c1f0a1301183060b58e3800422d39a46fe819
Kevin Tu <tukevin274@gmail.com>
2023-06-23 18:25:10 -0600

    Fixed conditions for generating max definitions.

--- definition-retriever.py
+++ definition-retriever.py
@@ -8,6 +8,8 @@ def get_definition(word):
     # sends a request to the dictionary API and stores the dictionary
     json_text = requests.get(Base_URL + word)
     # if the word is not found
+    maxDefs = 4
+    # Max amount of definitions to retrieve
     if json_text.status_code == 404:
         print("Word not found")
     else:
@@ -19,17 +21,19 @@ def get_definition(word):
             print(item["partOfSpeech"].capitalize()+ ":")
             for number, definitions in enumerate(item["definitions"]):
 
-                if number and len(definitions) == 4:
+                if (len(definitions) == 4) and (number <= maxDefs - 1):
                     # different partOfSpeech may not have any examples.
                     print(f"{number+1}. {definitions['definition']}")
                     print(f"Example: {definitions['example']}")
                     print("\n")
-                elif number > 4:
-                    # Some words may have many defs which will get redundant
-                    break
-                else:
+
+                elif number < 4:
                     print(f"{number+1}. {definitions['definition']}")
                     print("\n")
+                
+                else:
+                    # Some words may have many defs which will get redundant
+                    break
         
 def main():
     word = input("Enter a Word: ")
